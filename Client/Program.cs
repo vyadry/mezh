259920573f4715418cb7ffd98230e985b9ced042
Vyacheslav Dryuchin <vyacheslavdry@yandex-team.ru>
2020-03-22 23:23:22 +0300

    ver1

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,30 +1,154 @@
 ﻿using System;
-
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace tcp
 {
+    class Message
+    {
+        public string type;
+        public long value;
+        public string id;
+        public Message(string type, long value, string id)
+        {
+            this.type = type;
+            this.id = id;
+            this.value = value;
+        }
+         
+    }
+
+    class Result
+    {
+        public string id;
+        public long value;
+        public string type;
+        public long answer;
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static int port = 8005; // порт сервера
+        static string address = "127.0.0.1"; // адрес сервера
+
+        static public void DisplayMenu()
         {
-            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse("0.0.0.0"), 58900);
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.Connect(ipe);
-            if (!socket.Connected)
-            {
-                Console.WriteLine("Fail - Connection failed");
-                return;
-            }
-            Console.WriteLine("Connected!");
+            Console.Clear();
+            Console.WriteLine("Select Math orepation");
+            Console.WriteLine();
+            Console.WriteLine("1. Factorial");
+            Console.WriteLine("2. SumOfRow");
+            Console.WriteLine("3. GetResult");
+        }
+        static public long GetNumber(string title)
+        {
             while (true)
             {
+                Console.WriteLine(title);
+
+                long Num;
                 string str = Console.ReadLine();
-                Console.WriteLine(str);
-                socket.Send(Encoding.ASCII.GetBytes(str), Encoding.ASCII.GetBytes(str).Length, 0);
+
+                bool isNum = long.TryParse(str, out Num);
+
+                if (isNum)
+
+                {
+                    return Num;
+
+                }
+
             }
+
+        }
+        static public Message Menu()
+        {
+            string myid = "Client1";
+            while (true)
+            {
+                DisplayMenu();
+                Console.WriteLine();
+                long answer = GetNumber("Select action");
+                switch (answer)
+                {
+                    case 1:
+                        long value = GetNumber("Select factorial value:");
+                        return new Message("Factorial", value, myid);
+                    case 2:
+                        long value2 = GetNumber("Select SumOfRow value:");
+                        return new Message("SumOfRow", value2, myid);
+                    case 3:
+                        return new Message("GetResult", 0, myid);
+                    default:
+                        break;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press some button");
+            Console.ReadLine();
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse(address), port);
+            try
+            {
+                while (true)
+                {
+                    Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    socket.Connect(ipe);
+                    if (!socket.Connected)
+                    {
+                        Console.WriteLine("Fail - Connection failed");
+                        return;
+                    }
+                    Console.WriteLine("Connected!");
+                    Message newMessage = Menu();
+                    string json = JsonConvert.SerializeObject(newMessage);
+                    byte[] data = Encoding.Unicode.GetBytes(json);
+                    socket.Send(data);
+ 
+
+                    // получаем ответ
+                    data = new byte[256]; // буфер для ответа
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0; // количество полученных байт
+
+                    do
+                    {
+                        bytes = socket.Receive(data, data.Length, 0);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (socket.Available > 0);
+
+                    if (newMessage.type == "GetResult")
+                    {
+                        var result = JsonConvert.DeserializeObject<List<Result>>(builder.ToString());
+                        result.ForEach(el =>
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("operation: " + el.type);
+                            Console.WriteLine("value: " + el.value);
+                            Console.WriteLine("answer: " + el.answer);
+                        });
+                    }
+                    else
+                    {
+                        Console.WriteLine("server answer: " + builder.ToString());
+                    }
+
+                    // закрываем сокет
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                    Console.Read();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.Read();
         }
     }
 }
--- mezh/Server.cs
+++ mezh/Server.cs
@@ -2,39 +2,175 @@
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Threading;
 
 namespace tcp
 {
-    class Program
+    static class CalcOperations
     {
-        static void Main(string[] args)
+        public static long Factorial(long num)
         {
-            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse("0.0.0.0"), 58900);
+            long res = 1;
+            for (long i = num; i > 1; i--)
+                res *= i;
+            return res;
+        }
+        public static long SumRow(long num)
+        {
+            long res = 1;
+            for (long i = num; i > 1; i--)
+                res += i;
+            return res;
+        }
+    }
 
+
+    class MyTask
+    {
+        public string id;
+        public string type;
+        public long value;
+        public MyTask(string id, string type, long value)
+        {
+            this.id = id;
+            this.type = type;
+            this.value = value;
+        }
+    }
+    class Result
+    {
+        public string id;
+        public long value;
+        public string type;
+        public long answer;
+        public Result(string id, long value, string type, long answer)
+        {
+            this.id = id;
+            this.value = value;
+            this.type = type;
+            this.answer = answer;
+        }
+    }
+
+        class Server
+        {
+            ConcurrentQueue<MyTask> tasks = new ConcurrentQueue<MyTask>();
+            List<Result> results = new List<Result>();
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipe = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8005);
+            private readonly object listLock = new object();
 
-            socket.Bind(ipe);
-
-            socket.Listen(1);
-
-            Console.WriteLine("Done - Server started");
-            Console.WriteLine(ipe);
-
-
-            while (true)
+            public void CalculateQueue()
             {
-                Socket socket_ = socket.Accept();
-                int bytes = 0;
-                byte[] data = new byte[256];
-                do
+                while (true)
                 {
-                    bytes = socket_.Receive(data);
-                    Console.WriteLine($"Info - Received mess from {((IPEndPoint)(socket_.RemoteEndPoint)).Address.ToString()}:" + Encoding.ASCII.GetString(data, 0, bytes));
-                } while (bytes > 0);
+                    MyTask task;
+                    while (tasks.TryDequeue(out task))
+                    {
+                        if (task != null)
+                        {
+                            lock(listLock){
+                                results.Add(Calculate(task));
+                            }
+                        }
+                    }
+                    Thread.Sleep(0);
+                }
+            }
+            public Result Calculate(MyTask task)
+            {
+                switch (task.type)
+                {
+                    case "Factorial":
+                        return new Result(task.id, task.value, task.type, CalcOperations.Factorial(task.value));
+                    case "SumOfRow":
+                        return new Result(task.id, task.value, task.type, CalcOperations.SumRow(task.value));
+                }
+                throw new Exception("not found");
+            }
+            public void Start()
+            {
+                Thread myThread = new Thread(CalculateQueue);
+                myThread.Start();
+                try
+                {
+                    socket.Bind(ipe);
 
-                socket_.Shutdown(SocketShutdown.Both);
-                socket_.Close();
+                    socket.Listen(10);
+
+                    Console.WriteLine("Done - Server started");
+                    Console.WriteLine(ipe);
+
+
+                    while (true)
+                    {
+                        Socket handler = socket.Accept();
+
+                        // получаем сообщение
+                        StringBuilder builder = new StringBuilder();
+                        int bytes = 0; // количество полученных байтов
+                        byte[] data = new byte[256]; // буфер для получаемых данных
+
+                        do
+                        {
+                            bytes = handler.Receive(data);
+                            builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                        }
+                        while (handler.Available > 0);
+
+                        Console.WriteLine(DateTime.Now.ToShortTimeString() + ": " + builder.ToString());
+
+                        MyTask t = JsonConvert.DeserializeObject<MyTask>(builder.ToString());
+                        if (t.type == "Factorial" || t.type == "SumOfRow")
+                        {
+                            tasks.Enqueue(t);
+                            // отправляем ответ
+                            string message = "Операция добавлена в очередь";
+                            data = Encoding.Unicode.GetBytes(message);
+                            handler.Send(data);
+                        }
+                        else if (t.type == "GetResult")
+                        {
+                            
+                            var founded = results.FindAll(el => el.id == t.id);
+                            results.RemoveAll(el => el.id == t.id);
+                            // отправляем ответ
+                            string json = JsonConvert.SerializeObject(founded);
+                            data = Encoding.Unicode.GetBytes(json);
+                            handler.Send(data);
+                        }
+                        else
+                        {
+                            string message = "Ошибка";
+                            data = Encoding.Unicode.GetBytes(message);
+                        }
+
+                        // закрываем сокет
+
+                        handler.Shutdown(SocketShutdown.Both);
+                        handler.Close();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                Console.WriteLine("\nPress ENTER to continue...");
+                Console.Read();
+            }
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Server server = new Server();
+                server.Start();
+
             }
         }
     }
-}
